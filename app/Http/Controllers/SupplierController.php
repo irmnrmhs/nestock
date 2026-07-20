dc64bc0d12c028e906c6f4bf1ef3f54626268b44
irmnrmhs <irmnrmhs@gmail.com>
2026-07-20 11:53:22 +0700

    feat(supplier): implement supplier CRUD

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $suppliers = Supplier::latest()->get();
+        return view('masters.supplier', compact('suppliers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'supplier'  => 'required|string',
+            'kode'      => 'required|string|unique:suppliers,kode',
+            'ket'       => 'nullable|string',
+        ]);
+
+        $supplier = Supplier::create($validated);
+
+        // return response()->json([
+        //     'status' => 'success',
+        //     'message' => 'Data Supplier berhasil ditambahkan.',
+        // ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Supplier $supplier)
+    {
+        $supplier = Supplier::findOrFail($supplier);
+        return response()->json($supplier);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Supplier $supplier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Supplier $supplier)
+    {
+        $validated = $request->validate([
+            'supplier'  => 'required|string',
+            'kode'      => 'required|string|unique:suppliers,kode,'.$supplier,
+            'ket'       => 'nullable|string',
+        ]);
+
+        $supplier = Supplier::findOrFail($supplier);
+        $supplier->update($validated);
+
+        // return response()->json([
+        //         'status' => 'success',
+        //         'message' => 'Data Supplier berhasil diperbaharui.',
+        // ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Supplier $supplier)
+    {
+        $supplier = Supplier::findOrFail($supplier);
+        $supplier->delete();
+
+        // return response()->json([
+        //     'status' => 'success',
+        //     'message' => 'Data Supplier berhasil dihapus.',
+        // ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier.index');
+    // Route::post('/supplier', [SupplierController::class, 'store'])->name('supplier.store');
+    // Route::put('/supplier/{id}', [SupplierController::class, 'update'])->name('supplier.update');
+    // Route::delete('/supplier/{id}', [SupplierController::class, 'destroy'])->name('supplier.destroy');
+    Route::resource('supplier', SupplierController::class)
+        ->only(['index', 'store', 'update', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
